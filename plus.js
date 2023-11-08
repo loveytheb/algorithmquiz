d8fc57e7b3362795871c2ac80a2f074a260553ff
loveytheb <cjstjsn99@gamil.com>
2023-11-08 20:53:53 +0900

    입문 > 두 수의 합

---
+++ plus.js
@@ -0,0 +1,14 @@
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+
+function solution(num1, num2) {
+    // 1. num1과 num2의 범위 설정하기
+    if (-50000 <= num1 && 50000 >= num1)
+    if (-50000 <= num2 && 50000 >= num2)    
+    // 2. num1과 num2 합 return하도록 만들기
+    return num1 + num2;
+}
+
+console.log(solution);
