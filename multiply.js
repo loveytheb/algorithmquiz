b5796b02deca91c38d82693e9a426974ee74a5c4
loveytheb <cjstjsn99@gamil.com>
2023-11-08 20:54:53 +0900

    입문 > 두 수의 곱

---
+++ multiply.js
@@ -0,0 +1,15 @@
+// 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+// 제한사항
+// 0 ≤ num1 ≤ 100
+// 0 ≤ num2 ≤ 100
+
+function solution(num1, num2) {
+    // 1. num1과 num2 범위 설정하기
+    if(0 <= num1 && num1 <= 100)
+    if(0 <= num2 && num2 <= 100)
+
+    // 2. num1과 num2 곱한 값 return하기
+    return num1 * num2;
+}
+
+console.log(solution(3, 4)); // 12
