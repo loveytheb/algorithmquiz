27f3724637df3bb50ab8d2b04a00aa509099c57e
loveytheb <cjstjsn99@gamil.com>
2023-11-08 20:57:30 +0900

    입문 > 몫 구하기

---
+++ quotient.js
@@ -0,0 +1,15 @@
+// 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+// 제한사항
+// 0 < num1 ≤ 100
+// 0 < num2 ≤ 100
+
+function solution(num1, num2) {
+    // 1. num1과 num2 범위 설정하기
+    if(0 <= num1 && num1 <= 100)
+    if(0 <= num2 && num2 <= 100)
+
+    // 2. num1을 num2로 나눈 몫 return하기
+    return Math.floor(num1 / num2);
+};
+
+console.log(solution(7, 2));
