aa1cf61661d8ed2d017636f06ccbf25587b8e764
loveytheb <146186897+loveytheb@users.noreply.github.com>
2024-09-30 18:48:09 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 52 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -3,28 +3,27 @@ from collections import deque
 
 input = sys.stdin.readline
 
-n = int(input())  # 게임판 크기 입력
-graph = [list(map(int, input().split())) for _ in range(n)]  # 게임판 입력
-visited = [[0] * n for _ in range(n)]  # 방문 기록 배열 초기화
-dx = [0, 1]  # 오른쪽으로 이동
-dy = [1, 0]  # 아래쪽으로 이동
+n = int(input()) # 게임판 크기
+graph = [list(map(int, input().split())) for _ in range(n)] # N x N 크기의 게임판
+visited = [[0] * n for _ in range(n)] # 방문 기록 배열 초기화
+dx = [0, 1] # 오른쪽으로 이동
+dy = [1, 0] # 아래쪽으로 이동
 
 def bfs(x, y):
-    queue = deque([(x, y)]) # 시작점 (x, y)을 큐에 넣기
+    queue = deque([(x, y)]) # 시작점 (x, y)을 큐에 넣음
     while queue:
-        x, y = queue.popleft() # 큐의 맨 앞에 있는 좌표를 꺼내고
-        if (x, y) == (n - 1, n - 1): # 목표 지점에 도착했다면
-            return 1  # 1을 반환해 성공을 알립니다.
+        x, y = queue.popleft() # 큐의 맨 앞에 있는 좌표를 꺼냄
+        if (x, y) == (n - 1, n - 1): # 목표 지점에 도착했는지 확인
+            return 1 # 도착했다면 1을 반환
         
-        # 오른쪽, 아래쪽 두 방향에 대해 탐색합니다.
-        for i in range(2):
-            nx = x + dx[i] * graph[x][y]  # x좌표에서 이동
-            ny = y + dy[i] * graph[x][y]  # y좌표에서 이동
+        for i in range(2): # 오른쪽과 아래쪽 두 방향에 대해 탐색
+            nx = x + dx[i] * graph[x][y] # x축 이동
+            ny = y + dy[i] * graph[x][y] # y축 이동
             
-            # 범위 내에 있고 방문하지 않았다면 이동
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                queue.append((nx, ny))  # 새로 이동한 좌표를 큐에 넣기
-                visited[nx][ny] = 1  # 방문 기록을 남기기
-    return 0 # 끝까지 탐색했으나 도착하지 못한 경우 0 반환
+            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]: # 범위 내에 있고 방문하지 않았다면
+                queue.append((nx, ny)) # 큐에 새 좌표 추가
+                visited[nx][ny] = 1 # 해당 좌표를 방문했음을 기록
+    return 0 # 큐가 비었을 때까지 목표 지점에 도착하지 못했다면 0을 반환
+
 
 print("HaruHaru" if bfs(0, 0) else "Hing")
