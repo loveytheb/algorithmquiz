16c7508c8a16153adcc16cc2b6fbd5b5ef36651c
loveytheb <146186897+loveytheb@users.noreply.github.com>
2024-11-22 21:38:22 +0900

    [Silver IV] Title: 상근이의 여행, Time: 148 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,15 @@
+import sys
+from collections import defaultdict
+
+input = sys.stdin.readline
+
+T = int(input()) # 테스트 케이스 개수
+
+for _ in range(T):
+    # N: 국가의 수 M: 비행기의 종류
+    N, M = map(int, input().split())
+    
+    for _ in range(M):
+        a, b = map(int, input(). split())
+    
+    print(N - 1)
