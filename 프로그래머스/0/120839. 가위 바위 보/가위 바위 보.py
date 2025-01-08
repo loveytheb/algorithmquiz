a0322c976e61b85b217237058ce671e3f1773dba
loveytheb <146186897+loveytheb@users.noreply.github.com>
2025-01-08 22:45:00 +0900

    [level 0] Title: 가위 바위 보, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.py
@@ -0,0 +1,11 @@
+def solution(rsp):
+    result = ""
+    for i in str(rsp):
+        if i == '2':
+            result += '0'
+        elif i == '0':
+            result += '5'
+        elif i == '5':
+            result += '2'
+    
+    return result
